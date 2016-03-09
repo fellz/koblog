5a94174f4580072fb762f8782d71f0e0fad9075e
Roman <felixcon@gmail.com>
2016-03-09 18:14:56 +0300

    add articles stuff

---
+++ db/migrate/20160309145035_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :description
+      t.text :body
+      t.boolean :approved
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160309141049) do
+ActiveRecord::Schema.define(version: 20160309145035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.text     "body"
+    t.boolean  "approved"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "redactor_assets", force: :cascade do |t|
     t.integer  "user_id"
