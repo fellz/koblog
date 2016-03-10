391a4c8ab1d07ae1f6c6a82549d9412742b8bd16
Roman <felixcon@gmail.com>
2016-03-10 11:06:14 +0300

    views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160309161118) do
+ActiveRecord::Schema.define(version: 20160310075515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,10 @@ ActiveRecord::Schema.define(version: 20160309161118) do
     t.boolean  "approved"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "articles", ["user_id"], name: "index_articles_on_user_id", using: :btree
 
   create_table "redactor_assets", force: :cascade do |t|
     t.integer  "user_id"
