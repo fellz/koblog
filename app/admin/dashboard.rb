a368ea2dbd1206c12dfcbd22e50d5042b386ac98
Roman <felixcon@gmail.com>
2016-03-14 16:01:24 +0300

    customized admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,25 @@
 ActiveAdmin.register_page "Dashboard" do
 
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
-
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  
+  content :title => proc{ I18n.t("active_admin.dashboard") } do
+    columns do
+      column do
+        panel "Кол-во статей на главной" do
+          status_tag(Article.all.count)
+        end  
       end
-    end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+      column do
+        panel "Кол-во всех зарегистрированных пользователей" do
+          status_tag(User.all.count)
+        end
+      end
+      column do
+        panel "Кол-во зарегистрированных пользователей сегодня" do
+          users = User.select {|u| u.created_at.to_date == Date.today}
+          status_tag(users.empty? ? 'зеро' : users.count)
+        end
+      end
+    end    
   end # content
 end
