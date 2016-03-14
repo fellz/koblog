f7546a2f1c6120f1aae25749a710f6489f346cd6
Roman <felixcon@gmail.com>
2016-03-14 12:18:43 +0300

    some fixes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :categories
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
--- app/views/comments/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@comments) do |comment|
-  json.extract! comment, :id, :author
-  json.url comment_url(comment, format: :json)
-end
