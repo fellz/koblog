35f3c2faab81c39be366342f3f402d469e3d1124
Roman <felixcon@gmail.com>
2016-03-17 11:53:13 +0300

    forgot to add email setting to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,18 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+
+  config.action_mailer.default_url_options = {:host => 'koblogg.herokuapp.com'}  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :port      => 587,
+    :address   => "smtp.mandrillapp.com",
+    :user_name => "felixcon@gmail.com",
+    :password  => "PvgSXQ2VNN96ZDzlZ9CSHg"
+   }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
